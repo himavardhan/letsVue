7f4de433e1d6b895f7ae8a9c5b939ba47fd3a935
Himavardhan Reddy Donuru <himavardhan.d@gmail.com>
2026-02-18 21:58:55 -0600

    ADDED NEW VUE COMPONENTS

--- src/App.vue
+++ src/App.vue
@@ -1,47 +1,27 @@
 <script setup>
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
+import  IntroToVue  from '@/components/IntroToVue.vue'
+import { ref } from 'vue';
+import Header  from '@/components/Header.vue';
+import Footer from '@/components/Footer.vue';
+let name = ref('Guest')
+const year = ref(2014)
+
 </script>
 
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
+  <Header/>  
 
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-    </div>
-  </header>
+  <!-- <IntroToVue /> -->
 
-  <main>
-    <TheWelcome />
-  </main>
+  <Footer/>
+  
 </template>
 
 <style scoped>
-header {
-  line-height: 1.5;
-}
 
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
 
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
 
-  .logo {
-    margin: 0 2rem 0 0;
-  }
 
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-}
+
+
 </style>
