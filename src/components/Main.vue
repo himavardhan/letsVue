923d13f60c4e748b68739f50cd1c2b099bad8ad6
Himavardhan Reddy Donuru <himavardhan.d@gmail.com>
2026-02-24 11:24:44 -0600

    Added v-on event with@

--- src/components/Main.vue
+++ src/components/Main.vue
@@ -11,23 +11,27 @@ const changeNumber = (Type) => {
     number.value++;
      }
 }   
-const updateList = () => {
-    if(name.value.trim() === '') return;
-    list.value.push(name.value);
-    name.value = '';
+const updateList = (Type) => {
+    
+    if(list.value.length > 0 && Type === 'REMOVE'){
+        list.value.pop();
+    }else{
+       if(name.value.trim() === '') return;
+        list.value.push(name.value);
+        name.value = '';
+    }
 }
 </script>
 <template> 
-  <h1>Counter: {{ number }}</h1>
-  <input type="text" v-model="name" placeholder="Enter your name">
-    <p>Your name is: {{ name }}</p>
+<br>
+  <input type="text" v-model="name" placeholder="Enter your name"><br>
+    <p>Your list:</p><br>
     <ul>
       <li v-for="(item, index) in list" :key="index">{{ item }}</li>
     </ul>
-
-  <button @click="updateList()">Add to List</button>
-  <button @click="changeNumber('increase')">Increase</button>
-  <button @click="changeNumber('decrease')">Decrease</button>
+  <br>
+  <button @click="updateList('ADD')">Add to List</button><br>
+  <button @click="updateList('REMOVE')">Remove from List</button>
 </template>
 <style scoped>
 
